ee4cf92a647d1cffab8f5b65f01acaf76761fb29
Yugesh Poudel <yugman45d@gmail.com>
2024-05-25 14:53:37 +0545

    Gemini Page initialized

--- lib/config/app_constants.dart
+++ lib/config/app_constants.dart
@@ -5,4 +5,19 @@ class AppConstants {
 
 const String accessToken =
     "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNWFjMzQ3MGRjNzg4YmY5MGFkODkyZDQyMjMzNDc2OCIsInN1YiI6IjY1YTBlOGY3Njc4MjU5MDEyZmU4Yzk0OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hvJqQiiizeG_FXrBZWOpFA5qloqU8Rf_CtJt7bufJZw";
-    //!Yo yaha rakhne haina, temporarily yaha rakheko
+//!Yo yaha rakhne haina, temporarily yaha rakheko
+
+class GeminiConstants {
+  //!Hide this apiKey later on
+  static const String apiKey = 'AIzaSyA86iKJa9xpp7WfKdRpIHbVTKzAeuMQJKo';
+  static const String prompt =
+      'You are a chatbot specialized in the entertainment industry. Your task is to provide detailed and accurate answers to any questions related to movies, TV shows, and people involved in their production. If a question is not related to the entertainment industry, politely inform the user that you only answer entertainment-related questions. If you do not know the answer to a question, politely inform the user that you are unable to provide an answer. Remember to maintain a friendly and professional tone throughout the conversation. Enjoy chatting!';
+  static const String welcomeMessage =
+      'Hello! I am Gemini, your entertainment chatbot. How can I help you today?';
+  static const String unknownErrorMessage =
+      'I am sorry, but I am unable to provide an answer to that question. Please ask me something else.';
+  static const String invalidInputMessage =
+      'I am sorry, but I only answer entertainment-related questions. Please ask me something else.';
+  static const String loadingMessage =
+      'Please wait while I find the answer for you...';
+}
--- lib/demo_starting_page.dart
+++ lib/demo_starting_page.dart
@@ -1,9 +1,72 @@
+// import 'dart:developer';
+
+// import 'package:flutter/material.dart';
+// import 'package:go_router/go_router.dart';
+// import 'package:netflix/components/local_storage/watchlater_helper.dart';
+// import 'package:netflix/routes/app_route_constant.dart';
+
+// class DemoStartingPage extends StatefulWidget {
+//   const DemoStartingPage({super.key});
+
+//   @override
+//   State<DemoStartingPage> createState() => _DemoStartingPageState();
+// }
+
+// class _DemoStartingPageState extends State<DemoStartingPage> {
+//   // @override
+//   // void initState() {
+//   //   MyListHelper.clearMovieList();
+//   //   MyListHelper.clearTvShowList();
+//   //   super.initState();
+//   // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     ColorScheme myColorScheme = Theme.of(context).colorScheme;
+//     Size mySize = MediaQuery.sizeOf(context);
+//     TextTheme myTextTheme = Theme.of(context).textTheme;
+//     return Scaffold(
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Center(
+//             child: ElevatedButton(
+//               onPressed: () {
+//                 // log("Welcome to the World");
+//                 // GoRouter.of(context).pushNamed(
+//                 //   MyAppRouteConstants.practicePage,
+//                 // );
+//                 // GoRouter.of(context).pushNamed(
+//                 //   MyAppRouteConstants.loginPage,
+//                 // );
+//                 GoRouter.of(context).pushNamed(
+//                   MyAppRouteConstants.mainPage,
+//                 );
+//                 // GoRouter.of(context).pushNamed(
+//                 //   MyAppRouteConstants.peopleDetailsPage,
+//                 //   extra: 500,
+//                 // );
+//                 // GoRouter.of(context).pushNamed(
+//                 //   MyAppRouteConstants.tvShowDetailsPage,
+//                 //   extra: 1400,
+//                 // );
+//               },
+//               child: const Text('Login'),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+//!-------------------------Improve UI of the page-------------------------
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:netflix/components/local_storage/watchlater_helper.dart';
-import 'package:netflix/routes/app_route_constant.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+import 'package:netflix/config/app_constants.dart';
 
 class DemoStartingPage extends StatefulWidget {
   const DemoStartingPage({super.key});
@@ -13,49 +76,118 @@ class DemoStartingPage extends StatefulWidget {
 }
 
 class _DemoStartingPageState extends State<DemoStartingPage> {
-  // @override
-  // void initState() {
-  //   MyListHelper.clearMovieList();
-  //   MyListHelper.clearTvShowList();
-  //   super.initState();
-  // }
+  final inputController = TextEditingController();
+  bool isloading = false;
+
+  //--------------Setting up the model at first--------------------
+  final GenerativeModel model = GenerativeModel(
+    model: 'gemini-1.5-flash-latest',
+    apiKey: GeminiConstants.apiKey,
+  );
+
+  late final ChatSession session;
+
+  @override
+  void initState() {
+    session = model.startChat();
+    super.initState();
+    inputController.text = GeminiConstants.prompt;
+    sendMessage();
+  }
+  //-----------------Intialization part upto here-------------------
 
   @override
   Widget build(BuildContext context) {
-    ColorScheme myColorScheme = Theme.of(context).colorScheme;
-    Size mySize = MediaQuery.sizeOf(context);
-    TextTheme myTextTheme = Theme.of(context).textTheme;
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                // log("Welcome to the World");
-                // GoRouter.of(context).pushNamed(
-                //   MyAppRouteConstants.practicePage,
-                // );
-                // GoRouter.of(context).pushNamed(
-                //   MyAppRouteConstants.loginPage,
-                // );
-                GoRouter.of(context).pushNamed(
-                  MyAppRouteConstants.mainPage,
+      body: Column(children: [
+        Expanded(
+          child: ListView(
+            padding: const EdgeInsets.all(16.0),
+            children: [
+              ...session.history.map((content) {
+                var text = content.parts
+                    .whereType<TextPart>()
+                    .map<String>((e) => e.text)
+                    .join('');
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    isloading
+                        ? const Center(
+                            child: CircularProgressIndicator(),
+                          )
+                        : Align(
+                            alignment: Alignment.centerRight,
+                            child: MarkdownBody(
+                              data: text,
+                            ),
+                          ),
+                    const Divider(),
+                    const SizedBox(height: 10.0)
+                  ],
                 );
-                // GoRouter.of(context).pushNamed(
-                //   MyAppRouteConstants.peopleDetailsPage,
-                //   extra: 500,
-                // );
-                // GoRouter.of(context).pushNamed(
-                //   MyAppRouteConstants.tvShowDetailsPage,
-                //   extra: 1400,
-                // );
-              },
-              child: const Text('Login'),
-            ),
+              }),
+            ],
           ),
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: inputController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter your prompt',
+                  ),
+                  onEditingComplete: () {
+                    if (!isloading) {
+                      sendMessage();
+                    }
+                  },
+                ),
+              ),
+              IconButton(
+                onPressed: sendMessage,
+                icon: const Icon(Icons.send),
+              ),
+            ],
+          ),
+        )
+      ]),
     );
+  }
+
+  sendMessage() async {
+    setState(() {
+      isloading = true;
+    });
+    try {
+      final response =
+          await session.sendMessage(Content.text(inputController.text));
+      var text = response.text;
+      log(response.text.toString());
+      if (text == null) {
+        log("No response from the API");
+      } else {
+        setState(() {
+          isloading = false;
+        });
+      }
+    } catch (e) {
+      if (context.mounted) {
+        log(e.toString());
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.toString())));
+      }
+    } finally {
+      //Finally ma vayeko code, exception aayepani naaye pani run hunxa jasari ni
+      inputController.clear();
+      setState(() {
+        isloading = false;
+      });
+    }
   }
 }
